cc5ba071d7a281fd1f3b7588cec5e07e29b2b34b
OscarV123 <oscarvalderrama2006@gmail.com>
2025-07-07 06:23:20 -0500

    Se agrego la pantalla de iniciar sesion y el incono para la página dentro del navegador

---
+++ assets/js/iniciarSesion.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const mensaje = document.getElementById('mensaje');
+
+    if (email === "trabajador@senamhi.com") {
+        window.location.href = "trabajador_main.html";
+        return;
+    }
+
+    const registradoEmail = localStorage.getItem('airtrack_email');
+    const registradoPassword = localStorage.getItem('airtrack_password');
+    const tipo = localStorage.getItem('airtrack_perfil');
+    const nombre = localStorage.getItem('airtrack_nombre');
+
+    if (!email || !password) {
+        mensaje.textContent = "Por favor complete todos los campos.";
+        return;
+    }
+
+    if (email !== registradoEmail || password !== registradoPassword) {
+        mensaje.textContent = "Email o contraseña incorrectos.";
+        return;
+    }
+
+    // Guardar sesión
+    localStorage.setItem("airtrack_sesion_nombre", nombre);
+    localStorage.setItem("airtrack_sesion_tipo", tipo);
+
+    if (tipo === 'Vendedor ambulante') {
+        window.location.href = 'vendedor_main.html';
+    } else if (tipo === 'Deportista') {
+        window.location.href = 'deportista_main.html';
+    } else {
+        mensaje.textContent = "Tipo de usuario no reconocido.";
+    }
+});
